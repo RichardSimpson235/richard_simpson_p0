2dbe19c8ee2a4e4fbe684e334e3a158be85b9882
Richard Simpson <richard.simpson235@gmail.com>
2022-01-07 16:49:58 -0500

    Implemented LandingView

--- src/main/java/views/LandingView.java
+++ src/main/java/views/LandingView.java
@@ -1,36 +1,23 @@
 package main.java.views;
 
-import java.io.InputStream;
 import java.util.Scanner;
 
 public class LandingView extends AbstractView {
 
-    public LandingView(InputStream inputStream) {
-        super(inputStream);
-    }
-
-    /**
-     * This method renders the opening page view of the application. It is called by
-     * Application, which uses its return value to determine which page to navigate to.
-     *
-     */
     @Override
-    public void render() {
+    public String render() {
         System.out.println("Welcome to the Course Management Portal!");
         System.out.println("If you would like to log in, please enter 'login'.");
         System.out.println("If you would like to register, please enter 'register'.");
         System.out.println("If you would like to exit the portal, please enter 'exit'. " +
                             "You may enter exit at any time to exit the application.");
+
+        return listen();
     }
 
-    /**
-     * This method is used to listen for user input. It returns the value to render.
-     *
-     * @return       the cleaned string the user input into the console
-     */
     @Override
     public String listen() {
-        Scanner scanner = new Scanner(this.inputStream);
+        Scanner scanner = new Scanner(System.in);
 
         while(true) {
             String input = scanner.nextLine();
@@ -42,8 +29,6 @@ public class LandingView extends AbstractView {
                 System.out.println("You entered an invalid word! We got: '" + input
                                     + "', but we needed one of 'login', 'register', or 'exit'!");
             } else {
-                scanner.close();
-
                 return input;
             }
         }
