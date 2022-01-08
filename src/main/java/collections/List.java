39144b9925903863b6b04c2594ae56130c056290
Richard Simpson <richard.simpson235@gmail.com>
2022-01-08 11:50:59 -0500

    Add collections.List and implementation skeleton

---
+++ src/main/java/collections/List.java
@@ -0,0 +1,46 @@
+package main.java.collections;
+
+public class List<E> {
+
+    private Object[] array;
+    private int size;
+
+    public List() {
+        this.size = 2;
+        this.array = new Object[size];
+    }
+
+    public List(int size) {
+        this.size = size;
+        this.array = new Object[size];
+    }
+
+    @SuppressWarnings("unchecked")
+    public E get(int index) {
+        return (E) array[index];
+    }
+
+    public void add(E item) {}
+
+    public void remove(E item) {}
+
+    public boolean contains(E item) {}
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public String toString() {
+        String output = "List{";
+        for (Object item: this.array) {
+            output += item.toString();
+        }
+
+        return output + "}";
+    }
+}
