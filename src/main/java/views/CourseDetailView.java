cf6cd2e9cb6c021c8fea81200ccda2a827befbf4
Richard Simpson <richard.simpson235@gmail.com>
2022-01-08 14:16:42 -0500

    Add CourseDetailView

--- src/main/java/views/CourseDetailView.java
+++ src/main/java/views/CourseDetailView.java
@@ -1,84 +1,14 @@
 package main.java.views;
 
-import main.java.exceptions.InvalidDataException;
-import main.java.models.Course;
-import main.java.services.CourseEditService;
-
-import java.io.InputStream;
-import java.util.Scanner;
-
 public class CourseDetailView extends AbstractView {
 
-    private final Course course;
-    private final CourseEditService service;
-
-    public CourseDetailView(InputStream inputStream, CourseEditService service) {
-        super(inputStream);
-        this.service = service;
-
-        this.course = service.getCourse();
+    @Override
+    public String render() {
+        return null;
     }
 
     @Override
-    public void render() {
-        System.out.println("Detailed Class Viewer");
-        System.out.println(this.course);
-    }
-
-    @Override
-    public String listen() {
-
-        if(this.service.isUserFaculty()) {
-            System.out.println("If you would like to edit a field, enter the field's name (for example: 'name').");
-            System.out.println("If you would like to delete the class please enter 'delete'.");
-        } else {
-            if(this.service.isUserEnrolled()) {
-                System.out.println("If you would like to unenroll from this class type 'unenroll'.");
-            } else {
-                System.out.println("If you would like to enroll in this class, type 'enroll'");
-            }
-        }
-        Scanner scanner = new Scanner(this.inputStream);
-
-        while(true) {
-            String input = scanner.nextLine();
-
-            if(input.equalsIgnoreCase("exit")) {
-                scanner.close();
-
-                return input;
-            } else if(input.equalsIgnoreCase("unenroll")) {
-                service.unenroll();
-                scanner.close();
-
-                return "student";
-            } else if(input.equalsIgnoreCase("enroll")) {
-                service.enroll();
-                scanner.close();
-
-                return "student";
-            } else if(input.equalsIgnoreCase("delete")) {
-                service.deleteCourse(this.course);
-                System.out.println("Course: " + course.name + " was deleted!");
-                scanner.close();
-
-                return this.service.isUserFaculty() ? "faculty" : "student";
-            } else if(course.isValidField(input)) {
-                System.out.println("What would you like to change it to?");
-                String newFieldData = scanner.nextLine();
-
-                try {
-                    service.editCourse(course.ID, input, newFieldData);
-                    scanner.close();
-
-                    return this.service.isUserFaculty() ? "faculty" : "student";
-                } catch(InvalidDataException e) {
-                    System.out.println("The data you entered seems to be invalid. Please check it again.");
-                }
-            } else {
-                System.out.println("You entered an invalid option. We got: " + input + ", please check the valid inputs" +
-                        " again.");
-            }
-        }
+    protected String listen() {
+        return null;
     }
 }
