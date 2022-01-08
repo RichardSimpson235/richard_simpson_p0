8e27a1bd3f64cc39aeb1b2046b6f50ed227d5ca1
Richard Simpson <richard.simpson235@gmail.com>
2022-01-08 14:16:42 -0500

    Add CourseDetailView

---
+++ src/main/java/views/CourseDetailView.java
@@ -0,0 +1,14 @@
+package main.java.views;
+
+public class CourseDetailView extends AbstractView {
+
+    @Override
+    public String render() {
+        return null;
+    }
+
+    @Override
+    protected String listen() {
+        return null;
+    }
+}
