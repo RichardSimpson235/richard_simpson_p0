4c2a09621d6e8a2cb880aff71de12b96672c8196
Richard Simpson <richard.simpson235@gmail.com>
2022-01-08 14:14:22 -0500

    Add import for Course and initialize validSelections, made service and validSelections final

--- src/main/java/views/FacultyView.java
+++ src/main/java/views/FacultyView.java
@@ -1,10 +1,9 @@
 package main.java.views;
 
-import main.java.structures.List;
+import main.java.collections.List;
 import main.java.services.FacultyService;
 import main.java.models.Course;
 
-import java.io.InputStream;
 import java.util.Scanner;
 
 public class FacultyView extends AbstractView {
@@ -12,23 +11,22 @@ public class FacultyView extends AbstractView {
     private final FacultyService service;
     private final List<Integer> validSelections;
 
-    public FacultyView(InputStream inputStream, FacultyService service) {
-        super(inputStream);
+    public FacultyView(FacultyService service) {
         this.service = service;
 
         validSelections = new List<>();
     }
 
     @Override
-    public void render() {
+    public String render() {
         System.out.println("==========================================");
          List<Course> courses = service.getCourses();
 
         System.out.println("Welcome " + service.getFacultyName() + "!");
         if(courses.isEmpty()) {
-            System.out.println("You have not created any courses yet.");
+            System.out.println("You have not created any classes yet.");
         } else {
-            System.out.println("You have created the following courses:");
+            System.out.println("You have created the following classes:");
             for(int i = 1; i <=  courses.size(); i++) {
                 System.out.println(i + ". " + courses.get(i - 1));
                 validSelections.add(i);
@@ -40,42 +38,27 @@ public class FacultyView extends AbstractView {
         }
 
         System.out.println("If you would like to add a new class, please enter 'new'.");
+
+        return listen();
     }
 
     @Override
-    public String listen() {
-        Scanner scanner = new Scanner(this.inputStream);
+    protected String listen() {
+        Scanner scanner = new Scanner(System.in);
 
         while(true) {
             String input = scanner.nextLine();
 
             if (input.equalsIgnoreCase("exit")) {
-                scanner.close();
                 return input;
             }
 
-            try {
-                int index = Integer.parseInt(input);
-
-                if(!validSelections.contains((index))) {
-                    System.out.println("You entered an invalid number! We got: " + input +
-                                        ", we need one of: " + validSelections);
-                } else {
-                    String courseName = service.getCourses().get(index).name;
-                    System.out.println("You've selected: " + input + ". " + courseName + ". Is this correct? (y/n)");
-
-                    input = scanner.nextLine();
-                    if(input.equalsIgnoreCase("y")) {
-                        scanner.close();
-
-                        return String.valueOf(index);
-                    } else if(!input.equalsIgnoreCase("n")) {
-                        System.out.println("Please enter 'y' or 'n' for yes or no.");
-                    }
-                }
-            } catch (NumberFormatException e) {
-                System.out.println("It seems you didn't enter an integer. Please enter an integer.");
+            if (!validSelections.contains(Integer.parseInt(input))) {
+                System.out.println("You entered an invalid number! We got: " + input + ", we need one of: " + validSelections);
+            } else {
+                return input;
             }
+
         }
     }
 }
