f3c65ff6c136f9311aab698254945b0951188a1c
Richard Simpson <richard.simpson235@gmail.com>
2022-01-09 15:13:48 -0500

    Add and implement StudentView.

--- src/main/java/views/StudentView.java
+++ src/main/java/views/StudentView.java
@@ -1,6 +1,6 @@
 package main.java.views;
 
-import main.java.structures.List;
+import main.java.collections.List;
 import main.java.models.Course;
 import main.java.services.StudentService;
 
@@ -41,7 +41,7 @@ public class StudentView extends AbstractView {
     }
 
     @Override
-    public String listen() {
+    protected String listen() {
         Scanner scanner = new Scanner(this.inputStream);
         List<Course> courses = service.getCourses();
 
