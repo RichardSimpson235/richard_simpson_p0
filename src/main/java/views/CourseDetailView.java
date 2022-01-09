1fbcbd13f08e8e515c9dfd8eba7666e7de6c7700
Richard Simpson <richard.simpson235@gmail.com>
2022-01-09 15:26:32 -0500

    Add logic for dealing with when a student is viewing a class. It conditionally renders messages based on that, and handles enroll input. Change isFaculty to a User object for checking who the user is.

--- src/main/java/views/CourseDetailView.java
+++ src/main/java/views/CourseDetailView.java
@@ -2,6 +2,8 @@ package main.java.views;
 
 import main.java.exceptions.InvalidDataException;
 import main.java.models.Course;
+import main.java.models.Student;
+import main.java.models.User;
 import main.java.services.CourseEditService;
 
 import java.io.InputStream;
@@ -9,14 +11,14 @@ import java.util.Scanner;
 
 public class CourseDetailView extends AbstractView {
 
-    private final boolean isFaculty;
+    private final User user;
     private final Course course;
     private final CourseEditService service;
 
-    public CourseDetailView(InputStream inputStream, CourseEditService service, boolean isFaculty) {
+    public CourseDetailView(InputStream inputStream, CourseEditService service, User user) {
         super(inputStream);
         this.service = service;
-        this.isFaculty = isFaculty;
+        this.user = user;
 
         this.course = service.getCourse();
     }
@@ -29,8 +31,13 @@ public class CourseDetailView extends AbstractView {
 
     @Override
     protected String listen() {
-        System.out.println("If you would like to edit a field, enter the field's name (for example: 'name').");
-        System.out.println("If you would like to delete the class please enter 'delete'.");
+
+        if(this.user.isFaculty()) {
+            System.out.println("If you would like to edit a field, enter the field's name (for example: 'name').");
+            System.out.println("If you would like to delete the class please enter 'delete'.");
+        } else {
+            System.out.println("If you would like to unenroll from this class type 'unenroll'.");
+        }
         Scanner scanner = new Scanner(this.inputStream);
 
         while(true) {
@@ -40,12 +47,16 @@ public class CourseDetailView extends AbstractView {
                 scanner.close();
 
                 return input;
+            } else if(input.equalsIgnoreCase("unenroll")) {
+                service.unenrollStudent((Student) user);
+
+                return "student";
             } else if(input.equalsIgnoreCase("delete")) {
                 service.deleteCourse(this.course);
                 System.out.println("Course: " + course.name + " was deleted!");
                 scanner.close();
 
-                return isFaculty ? "faculty" : "student";
+                return this.user.isFaculty() ? "faculty" : "student";
             } else if(course.isValidField(input)) {
                 System.out.println("What would you like to change it to?");
                 String newFieldData = scanner.nextLine();
@@ -54,7 +65,7 @@ public class CourseDetailView extends AbstractView {
                     service.editCourse(course.ID, input, newFieldData);
                     scanner.close();
 
-                    return isFaculty ? "faculty" : "student";
+                    return this.user.isFaculty() ? "faculty" : "student";
                 } catch(InvalidDataException e) {
                     System.out.println("The data you entered seems to be invalid. Please check it again.");
                 }
