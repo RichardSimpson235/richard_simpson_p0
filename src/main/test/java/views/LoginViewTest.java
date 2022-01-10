0fccb8dbf6876ab4e453a2c65617d1eb781078bc
Richard Simpson <richard.simpson235@gmail.com>
2022-01-10 14:42:31 -0500

    Add and implement LoginViewTest and MockLoginService. Passes all tests.

---
+++ src/main/test/java/views/LoginViewTest.java
@@ -0,0 +1,58 @@
+package main.test.java.views;
+
+import main.java.exceptions.AuthenticationException;
+import main.java.views.LoginView;
+import main.test.java.mocks.MockLoginService;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.io.ByteArrayInputStream;
+import java.util.NoSuchElementException;
+
+public class LoginViewTest {
+
+    @Test
+    @Disabled
+    public void testRender() {
+        ByteArrayInputStream input = new ByteArrayInputStream("login".getBytes());
+        MockLoginService mockService = new MockLoginService();
+        LoginView view = new LoginView(input, mockService);
+        view.render();
+        assert true;
+    }
+
+    @Test
+    public void testListenWithStudent() {
+        ByteArrayInputStream input = new ByteArrayInputStream("student\npassword".getBytes());
+        MockLoginService mockService = new MockLoginService();
+        LoginView view = new LoginView(input, mockService);
+
+        assertEquals(view.listen(), "student");
+    }
+
+    @Test
+    public void testListenWithFaculty() {
+        ByteArrayInputStream input = new ByteArrayInputStream("faculty\npassword".getBytes());
+        MockLoginService mockService = new MockLoginService();
+        LoginView view = new LoginView(input, mockService);
+
+        assertEquals(view.listen(), "faculty");
+    }
+
+    // It throws a NoSuchElementException because there is no next line in the input
+    // ensure that the error message was printed first though.
+    @Test
+    @DisplayName("Should print a message to console when authentication fails.")
+    public void testListenAuthenticationFailure() {
+        ByteArrayInputStream input = new ByteArrayInputStream("asdf\npassword".getBytes());
+        MockLoginService mockService = new MockLoginService();
+        LoginView view = new LoginView(input, mockService);
+
+        assertThrows(NoSuchElementException.class, () -> {
+            view.listen();
+        });
+    }
+}
