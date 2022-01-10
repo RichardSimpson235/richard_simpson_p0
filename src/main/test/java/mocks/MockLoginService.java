326085df4799612e7ef43f8eb07e13d16f10aec8
Richard Simpson <richard.simpson235@gmail.com>
2022-01-10 15:51:50 -0500

    Add Override annotations

--- src/main/test/java/mocks/MockLoginService.java
+++ src/main/test/java/mocks/MockLoginService.java
@@ -5,6 +5,7 @@ import main.java.services.LoginService;
 
 public class MockLoginService extends LoginService {
 
+    @Override
     public String authenticate(String username, String password) throws AuthenticationException {
 
         if(username.equalsIgnoreCase("student") || username.equalsIgnoreCase("faculty")) {
