e755187c17e3caa90d60681a1a7b5f0237e72c92
Richard Simpson <richard.simpson235@gmail.com>
2022-01-10 15:39:36 -0500

    Add AccountRegistrationViewTest and MockRegistrationService. AccountRegistrationView passes all tests.

---
+++ src/main/test/java/mocks/MockRegistrationService.java
@@ -0,0 +1,8 @@
+package main.test.java.mocks;
+
+import main.java.services.RegistrationService;
+
+public class MockRegistrationService extends RegistrationService {
+
+    public void register(String firstName, String lastName, String age) {}
+}
