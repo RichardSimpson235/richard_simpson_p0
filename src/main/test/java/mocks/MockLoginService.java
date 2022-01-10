d914ca6538c59d9d43e6b29acabb64a3bdf07e96
Richard Simpson <richard.simpson235@gmail.com>
2022-01-10 14:42:31 -0500

    Add and implement LoginViewTest and MockLoginService. Passes all tests.

---
+++ src/main/test/java/mocks/MockLoginService.java
@@ -0,0 +1,16 @@
+package main.test.java.mocks;
+
+import main.java.exceptions.AuthenticationException;
+import main.java.services.LoginService;
+
+public class MockLoginService extends LoginService {
+
+    public String authenticate(String username, String password) throws AuthenticationException {
+
+        if(username.equalsIgnoreCase("student") || username.equalsIgnoreCase("faculty")) {
+            return username;
+        } else {
+            throw new AuthenticationException("bad username");
+        }
+    }
+}
