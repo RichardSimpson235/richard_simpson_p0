fa3fd7b13a228da39897768c0a549e2903d7d32f
Richard Simpson <richard.simpson235@gmail.com>
2022-01-12 10:29:11 -0500

    Create single arg and full arg constructors

--- src/main/java/structures/Context.java
+++ src/main/java/structures/Context.java
@@ -1,11 +1,24 @@
 package main.java.structures;
 
+import main.java.models.Course;
+import main.java.models.User;
+
 /**
  * This class is used to apply a context to view switching.
  * It allows different services ot know which User is using
  * the application, and which Course they're manipulating.
  */
 public class Context {
+
     public User user;
     public Course course;
+
+    public Context(User user) {
+        this.user = user;
+    }
+
+    public Context(User user, Course course) {
+        this.user = user;
+        this.course = course;
+    }
 }
