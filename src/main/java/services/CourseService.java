82983000450bfeab269a9ad9f1502e9dbcc8690c
Richard Simpson <richard.simpson235@gmail.com>
2022-01-12 17:07:03 -0500

    Add getCourse, getAllCourses, selectCourse, isUserFaculty, isUserEnrolled methods to let the different course views access the data they need to render.

--- src/main/java/services/CourseService.java
+++ src/main/java/services/CourseService.java
@@ -1,7 +1,10 @@
 package main.java.services;
 
 import main.java.exceptions.FailedEnrollmentException;
+import main.java.exceptions.InvalidDataException;
+import main.java.models.Course;
 import main.java.repositories.CourseRepository;
+import main.java.structures.List;
 
 public class CourseService {
 
@@ -13,15 +16,15 @@ public class CourseService {
         this.courseRepository = courseRepository;
     }
 
-    public void createCourse(String courseName, String description, long enrollmentPeriod) {
-        this.courseRepository.create(courseName, description, enrollmentPeriod);
+    public void createCourse(String courseName, String description, long enrollmentPeriodStart, long enrollmentPeriodEnd) {
+        this.courseRepository.create(courseName, description, enrollmentPeriodStart, enrollmentPeriodEnd);
     }
 
     public void deleteCourse() {
         this.courseRepository.delete(this.contextService.getContext().course);
     }
 
-    public void editCourse(String fieldName, String fieldData) {
+    public void editCourse(String fieldName, String fieldData) throws InvalidDataException {
         this.courseRepository.edit(this.contextService.getContext().course, fieldName, fieldData);
     }
 
@@ -31,5 +34,28 @@ public class CourseService {
 
     public void unenrollUser() throws FailedEnrollmentException {
         this.courseRepository.unenrollUser(this.contextService.getContext().user, this.contextService.getContext().course);
+    }
+
+    public Course getCourse() {
+        return this.contextService.getContext().course;
+    }
+
+    public List<Course> getAllCourses() {
+        return this.courseRepository.getAllCourses();
+    }
+
+    public void selectCourse(int index) {
+        contextService.setCourse(getAllCourses().get(index));
+    }
+
+    public boolean isUserFaculty() {
+        return this.contextService.getContext().user.isFaculty();
+    }
+
+    public boolean isUserEnrolled() {
+        Course course = this.contextService.getContext().course;
+        List<Course> enrolledCourses = this.contextService.getContext().user.getCourses();
+
+        return enrolledCourses.contains(course);
     }
 }
