050503c3fb2f79d3bdf6704300d36c1a6cf9a713
Richard Simpson <richard.simpson235@gmail.com>
2022-01-12 17:07:31 -0500

    Remove old mocks to work with new services.

--- src/main/test/java/mocks/MockRegistrationService.java
+++
@@ -1,9 +0,0 @@
-package main.test.java.mocks;
-
-import main.java.services.RegistrationService;
-
-public class MockRegistrationService extends RegistrationService {
-
-    @Override
-    public void register(String firstName, String lastName, String age, String username, String password) {}
-}
