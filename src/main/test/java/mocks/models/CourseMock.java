04c95437b29026811d8b4382641544c45d0c1084
Richard Simpson <richard.simpson235@gmail.com>
2022-01-13 11:11:44 -0500

    Add and implement StudentViewTest and relevant mocks. All tests pass.

---
+++ src/main/test/java/mocks/models/CourseMock.java
@@ -0,0 +1,11 @@
+package main.test.java.mocks.models;
+
+import main.java.models.Course;
+
+public class CourseMock extends Course {
+
+    @Override
+    public String toString() {
+        return "mock course";
+    }
+}
