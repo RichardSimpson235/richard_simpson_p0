70229408cc494eaac9ef5c725d1818a159e3555c
Richard Simpson <richard.simpson235@gmail.com>
2022-01-13 12:18:32 -0500

    Change to use exception catch instead of if statement.

--- src/main/java/views/CourseRegistryView.java
+++ src/main/java/views/CourseRegistryView.java
@@ -10,12 +10,10 @@ import java.util.Scanner;
 public class CourseRegistryView extends AbstractView {
 
     private final CourseService service;
-    private final List<Integer> validSelections;
 
     public CourseRegistryView(InputStream inputStream, CourseService service) {
         super(inputStream);
         this.service = service;
-        this.validSelections = new List<>();
     }
 
     @Override
@@ -24,8 +22,7 @@ public class CourseRegistryView extends AbstractView {
 
         List<Course> courses = service.getAllCourses();
         for(int i = 1; i <= courses.size(); i++) {
-            System.out.println(courses.get(i));
-            validSelections.add(i);
+            System.out.println(i + ". " + courses.get(i - 1));
         }
 
         System.out.println("Please enter the integer of its spot in the list (ex '1')");
@@ -42,12 +39,19 @@ public class CourseRegistryView extends AbstractView {
                 return input;
             } else {
                 int index = Integer.parseInt(input);
-                if(this.validSelections.contains(index)) {
-                    service.selectCourse(index);
-                    scanner.close();
+                try {
+                    Course course = service.getAllCourses().get(index - 1);
+                    System.out.println("You've selected: " +  input + " " + course.getName() + ", is this correct? (y/n)");
+                    input = scanner.nextLine();
+                    if(input.equalsIgnoreCase("y")) {
+                        scanner.close();
+                        service.selectCourse(index);
 
-                    return "detail";
-                } else {
+                        return "detail";
+                    } else if(!input.equalsIgnoreCase("n")) {
+                        System.out.println("Please enter 'y' or 'n' for yes or no.");
+                    }
+                } catch (IndexOutOfBoundsException e) {
                     System.out.println("We got an invalid integer, please check that you entered the correct integer!");
                 }
             }
