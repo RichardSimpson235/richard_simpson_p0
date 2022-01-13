453a8e6586742d56774f225d14c584c5aa2b8221
Richard Simpson <richard.simpson235@gmail.com>
2022-01-13 11:31:45 -0500

    Add conditional rendering of edit message for when there are classes to edit. Refactor input checking to use exception catching instead of if statements.

--- src/main/java/views/FacultyView.java
+++ src/main/java/views/FacultyView.java
@@ -10,13 +10,10 @@ import java.util.Scanner;
 public class FacultyView extends AbstractView {
 
     private final AccountService service;
-    private final List<Integer> validSelections;
 
     public FacultyView(InputStream inputStream, AccountService service) {
         super(inputStream);
         this.service = service;
-
-        validSelections = new List<>();
     }
 
     @Override
@@ -31,12 +28,13 @@ public class FacultyView extends AbstractView {
             System.out.println("You have created the following courses:");
             for(int i = 1; i <=  courses.size(); i++) {
                 System.out.println(i + ". " + courses.get(i - 1));
-                validSelections.add(i);
             }
 
-            System.out.println("If you would like to edit one of your classes, please enter an integer in the list " +
-                                "(for example press 1 for " +
-                                courses.get(0) + (courses.size() > 1 ? ", 2 for " + courses.get(1) + ", etc)." : ""));
+            if(courses.size() != 0) {
+                System.out.println("If you would like to edit one of your classes, please enter an integer in the list " +
+                        "(for example press 1 for " +
+                        courses.get(0) + (courses.size() > 1 ? ", 2 for " + courses.get(1) + ", etc)." : ")"));
+            }
         }
 
         System.out.println("If you would like to add a new class, please enter 'new'.");
@@ -49,7 +47,7 @@ public class FacultyView extends AbstractView {
         while(true) {
             String input = scanner.nextLine();
 
-            if (input.equalsIgnoreCase("exit")) {
+            if (input.equalsIgnoreCase("exit") || input.equalsIgnoreCase("new")) {
                 scanner.close();
                 return input;
             }
@@ -57,21 +55,21 @@ public class FacultyView extends AbstractView {
             try {
                 int index = Integer.parseInt(input);
 
-                if(!validSelections.contains((index))) {
-                    System.out.println("You entered an invalid number! We got: " + input +
-                                        ", we need one of: " + validSelections);
-                } else {
-                    String courseName = service.getCourses().get(index).getName();
-                    System.out.println("You've selected: " + input + ". " + courseName + ". Is this correct? (y/n)");
+                try {
+                    Course course = service.getCourses().get(index - 1);
+                    System.out.println("You've selected: " + input + ". " + course.getName() + ". Is this correct? (y/n)");
 
                     input = scanner.nextLine();
                     if(input.equalsIgnoreCase("y")) {
                         scanner.close();
+                        service.viewCourse(course);
 
-                        return String.valueOf(index);
+                        return "detail";
                     } else if(!input.equalsIgnoreCase("n")) {
                         System.out.println("Please enter 'y' or 'n' for yes or no.");
                     }
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("You entered an invalid number! Please check the integers in front of the classes.");
                 }
             } catch (NumberFormatException e) {
                 System.out.println("It seems you didn't enter an integer. Please enter an integer.");
