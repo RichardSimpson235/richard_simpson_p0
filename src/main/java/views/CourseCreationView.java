ae1b6152d0718bdca428d857985e1c95be148eef
Richard Simpson <richard.simpson235@gmail.com>
2022-01-17 17:43:20 -0500

    Refactor code to re-prompt user only when they mess up date formatting. Add handling for turning dates into longs as well as prompts for new fields.

--- src/main/java/views/CourseCreationView.java
+++ src/main/java/views/CourseCreationView.java
@@ -1,5 +1,7 @@
 package main.java.views;
 
+import main.java.exceptions.CreationFailedException;
+import main.java.services.AccountService;
 import main.java.services.CourseService;
 
 import java.io.InputStream;
@@ -10,11 +12,13 @@ import java.util.Scanner;
 
 public class CourseCreationView extends AbstractView {
 
-    private final CourseService service;
+    private final AccountService accountService;
+    private final CourseService courseService;
 
-    public CourseCreationView(InputStream inputStream, CourseService service) {
+    public CourseCreationView(InputStream inputStream, AccountService accountService, CourseService courseService) {
         super(inputStream);
-        this.service = service;
+        this.accountService = accountService;
+        this.courseService = courseService;
     }
 
     @Override
@@ -26,24 +30,26 @@ public class CourseCreationView extends AbstractView {
     public String listen() {
         Scanner scanner = new Scanner(this.inputStream);
 
-        while (true) {
-            System.out.println("What would you like to name this class?");
-            String name = scanner.nextLine();
-            if (name.equalsIgnoreCase("exit")) {
-                scanner.close();
+        System.out.println("What would you like to name this class?");
+        String name = scanner.nextLine();
+        if (name.equalsIgnoreCase("exit")) {
+            scanner.close();
 
-                return name;
-            }
+            return name;
+        }
 
-            System.out.println("Please enter a description for this class:");
-            String description = scanner.nextLine();
-            if (description.equalsIgnoreCase("exit")) {
-                scanner.close();
+        System.out.println("Please enter a description for this class:");
+        String description = scanner.nextLine();
+        if (description.equalsIgnoreCase("exit")) {
+            scanner.close();
 
-                return description;
-            }
+            return description;
+        }
 
-            System.out.println("When does enrollment for this class start? Enter date: MM/dd/yyyy");
+        DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+        System.out.println("When does enrollment for this class start? Enter date: MM/dd/yyyy");
+        long enrollmentStartDateL;
+        while(true) {
             String enrollmentStartDate = scanner.nextLine();
             if (enrollmentStartDate.equalsIgnoreCase("exit")) {
                 scanner.close();
@@ -51,7 +57,17 @@ public class CourseCreationView extends AbstractView {
                 return enrollmentStartDate;
             }
 
-            System.out.println("When does enrollment for this class end? Enter date: MM/dd/yyyy");
+            try {
+                enrollmentStartDateL = dateFormat.parse(enrollmentStartDate).getTime();
+                break;
+            } catch (ParseException e) {
+                System.out.println("It seems the date you entered was formatted incorrectly, please try again.");
+            }
+        }
+
+        System.out.println("When does enrollment for this class end? Enter date: MM/dd/yyyy");
+        long enrollmentEndDateL;
+        while(true) {
             String enrollmentEndDate = scanner.nextLine();
             if (enrollmentEndDate.equalsIgnoreCase("exit")) {
                 scanner.close();
@@ -59,19 +75,38 @@ public class CourseCreationView extends AbstractView {
                 return enrollmentEndDate;
             }
 
-            DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
-
             try {
-                long enrollmentStartDateLong = dateFormat.parse(enrollmentStartDate).getTime();
-                long enrollmentEndDateLong = dateFormat.parse(enrollmentEndDate).getTime();
-
-                service.createCourse(name, description, enrollmentStartDateLong, enrollmentEndDateLong);
-
-                scanner.close();
-                return "faculty";
+                enrollmentEndDateL = dateFormat.parse(enrollmentEndDate).getTime();
+                break;
             } catch (ParseException e) {
-                System.out.println("The dates seems to have been in an invalid format, please try again!");
+                System.out.println("It seems the date you entered was formatted incorrectly, please try again.");
             }
         }
+
+        System.out.println("How many credits is this course worth?");
+        int credits;
+        while(true) {
+            String creditInput = scanner.nextLine();
+            if (creditInput.equalsIgnoreCase("exit")) {
+                scanner.close();
+
+                return creditInput;
+            }
+
+            try {
+                credits = Integer.parseInt(creditInput);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Please be sure to only enter an integer, for example '1'");
+            }
+        }
+
+        try {
+            courseService.createCourse(name, description, enrollmentStartDateL, enrollmentEndDateL, credits);
+        } catch (CreationFailedException e) {
+            System.out.println("We failed to create the course, you may need to try again later.");
+        }
+
+        return "faculty";
     }
 }
