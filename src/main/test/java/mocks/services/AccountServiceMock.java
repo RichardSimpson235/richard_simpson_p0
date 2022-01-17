d8759fa223ad1af348c502af717b4161e54d841d
Richard Simpson <richard.simpson235@gmail.com>
2022-01-17 17:38:51 -0500

    Add date checking when trying to enroll in a class. Add three helper methods for displaying information.

--- src/main/test/java/mocks/services/AccountServiceMock.java
+++ src/main/test/java/mocks/services/AccountServiceMock.java
@@ -1,47 +1,128 @@
 package main.test.java.mocks.services;
 
-import main.java.exceptions.AuthenticationFailedException;
+import main.java.exceptions.CreationFailedException;
+import main.java.exceptions.EnrollmentFailedException;
+import main.java.exceptions.MissingAccountException;
 import main.java.exceptions.RegistrationFailedException;
 import main.java.models.Course;
+import main.java.models.Faculty;
+import main.java.models.Student;
+import main.java.models.User;
+import main.java.repositories.CourseRepository;
+import main.java.repositories.EnrollmentRepository;
+import main.java.repositories.UserRepository;
 import main.java.services.AccountService;
 import main.java.structures.List;
-import main.test.java.mocks.models.CourseMock;
 
 public class AccountServiceMock extends AccountService {
 
-    public AccountServiceMock() {
-        super(null, null);
+    private String flag;
+
+    public AccountServiceMock(UserRepository userRepository, CourseRepository courseRepository, EnrollmentRepository enrollmentRepository, String flag) {
+        super(userRepository, courseRepository, enrollmentRepository);
+        this.flag = flag;
     }
 
     @Override
-    public String authenticate(String username, String password) throws AuthenticationFailedException {
-        if(username.equals("student") && password.equals("password")) {
-            return "student";
-        } else if(username.equals("faculty") && password.equals("password")) {
-            return "faculty";
+    public void assign(Course course) throws CreationFailedException {
+    }
+
+    @Override
+    public String authenticate(String username, String password) throws MissingAccountException {
+        if(this.flag.equals(username)) {
+            return flag;
         } else {
-            throw new AuthenticationFailedException();
+            throw new MissingAccountException();
         }
     }
 
     @Override
-    public void register(String firstName, String lastName, String age, String username, String password) throws RegistrationFailedException {
+    public void register(String firstName, String lastName, String email, long dateOfBirth, String username, String password, int mealPlanTier, String major) throws RegistrationFailedException {
+    }
+
+    @Override
+    public void enroll(Course course) throws EnrollmentFailedException {
+    }
+
+    @Override
+    public void unenroll(Course course) throws EnrollmentFailedException {
     }
 
     @Override
     public List<Course> getCourses() {
-        List<Course> list = new List<>();
-        list.add(new CourseMock());
+        Faculty faculty = new Faculty();
+        faculty.setUserId(1);
+        faculty.setFirstName("first");
+        faculty.setLastName("last");
+        faculty.setEmail("first@gmail.com");
+        faculty.setDateOfBirth(0L);
+        faculty.setUsername("username");
+        faculty.setPassword("password");
+        faculty.setSalary(10);
+        faculty.setDepartment("DEPT");
 
-        return list;
+        Student student = new Student();
+        student.setUserId(3);
+        student.setFirstName("first");
+        student.setLastName("last");
+        student.setEmail("first@gmail.com");
+        student.setDateOfBirth(0L);
+        student.setUsername("username");
+        student.setPassword("password");
+        student.setMealPlanTier(10);
+        student.setMajor("DEPT");
+
+        Course course = new Course();
+        course.setCourseId(1);
+        course.setName("course1");
+        course.setCourseId(1);
+        course.setProfessor(faculty);
+        course.addStudent(student);
+
+        List<Course> courses = new List<>();
+        courses.add(course);
+
+        return courses;
     }
 
     @Override
-    public String getAccountName() {
-        return "student name";
+    public String getUserName() {
+        return "Mockery";
     }
 
     @Override
-    public void viewCourse(Course course) {
+    public boolean isUserFaculty() {
+        return this.flag.equals("faculty");
+    }
+
+    @Override
+    public User getUser() {
+        if(this.flag.equals("faculty")) {
+            Faculty faculty = new Faculty();
+            faculty.setUserId(1);
+            faculty.setFirstName("first");
+            faculty.setLastName("last");
+            faculty.setEmail("first@gmail.com");
+            faculty.setDateOfBirth(0L);
+            faculty.setUsername("username");
+            faculty.setPassword("password");
+            faculty.setSalary(10);
+            faculty.setDepartment("DEPT");
+
+            return faculty;
+        } else {
+            Student student = new Student();
+            student.setUserId(3);
+            student.setFirstName("first");
+            student.setLastName("last");
+            student.setEmail("first@gmail.com");
+            student.setDateOfBirth(0L);
+            student.setUsername("username");
+            student.setPassword("password");
+            student.setMealPlanTier(10);
+            student.setMajor("DEPT");
+
+            return student;
+        }
     }
 }
