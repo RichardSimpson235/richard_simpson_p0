bf0275f5e02b3c45d909947a7add23717b623995
Richard Simpson <richard.simpson235@gmail.com>
2022-01-17 17:44:39 -0500

    Add helper method for rendering courses.

--- src/main/java/views/CourseRegistryView.java
+++ src/main/java/views/CourseRegistryView.java
@@ -1,5 +1,6 @@
 package main.java.views;
 
+import main.java.exceptions.CourseRetrievalException;
 import main.java.structures.List;
 import main.java.models.Course;
 import main.java.services.CourseService;
@@ -10,6 +11,7 @@ import java.util.Scanner;
 public class CourseRegistryView extends AbstractView {
 
     private final CourseService service;
+    private List<Course> courses;
 
     public CourseRegistryView(InputStream inputStream, CourseService service) {
         super(inputStream);
@@ -20,13 +22,19 @@ public class CourseRegistryView extends AbstractView {
     public void render() {
         System.out.println("The following is a list of all of the classes:");
 
-        List<Course> courses = service.getAllCourses();
-        for(int i = 1; i <= courses.size(); i++) {
-            System.out.println(i + ". " + courses.get(i - 1));
-        }
+        try {
+            this.courses = service.getAllCourses();
+            for(int i = 1; i <= courses.size(); i++) {
+                renderCourse(this.courses.get(i - 1));
+            }
 
-        System.out.println("Please enter the integer of its spot in the list (ex '1')");
+            System.out.println("Please enter the integer of its spot in the list (ex '1')");
+        } catch (CourseRetrievalException | IndexOutOfBoundsException e) {
+            System.out.println("The registry seems to be having some problems, try again later. To exit type, 'exit'.");
+        }
     }
+
+    private void renderCourse(Course course) {}
 
     @Override
     public String listen() {
@@ -40,12 +48,12 @@ public class CourseRegistryView extends AbstractView {
             } else {
                 int index = Integer.parseInt(input);
                 try {
-                    Course course = service.getAllCourses().get(index - 1);
+                    Course course = this.courses.get(index - 1);
                     System.out.println("You've selected: " +  input + " " + course.getName() + ", is this correct? (y/n)");
                     input = scanner.nextLine();
                     if(input.equalsIgnoreCase("y")) {
                         scanner.close();
-                        service.selectCourse(index);
+                        service.selectCourse(course);
 
                         return "detail";
                     } else if(!input.equalsIgnoreCase("n")) {
