34023bce03d93203b0e7ee2234ee976b4f0c9728
Richard Simpson <richard.simpson235@gmail.com>
2022-01-17 17:47:45 -0500

    Add CourseService dependency because ContextService was removed. Views now call services directly when they need something. Added renderCourse helper method for rendering courses.

--- src/main/java/views/StudentView.java
+++ src/main/java/views/StudentView.java
@@ -1,5 +1,6 @@
 package main.java.views;
 
+import main.java.services.CourseService;
 import main.java.structures.List;
 import main.java.models.Course;
 import main.java.services.AccountService;
@@ -9,39 +10,45 @@ import java.util.Scanner;
 
 public class StudentView extends AbstractView {
 
-    private final AccountService service;
+    private final AccountService accountService;
+    private final CourseService courseService;
+    private List<Course> courses;
 
-    public StudentView(InputStream inputStream, AccountService service) {
+    public StudentView(InputStream inputStream, AccountService accountService, CourseService courseService) {
         super(inputStream);
-        this.service = service;
+        this.accountService = accountService;
+        this.courseService = courseService;
     }
 
     @Override
     public void render() {
         System.out.println("==========================================");
-        List<Course> courses = service.getCourses();
+        this.courses = accountService.getCourses();
 
-        System.out.println("Welcome " + service.getAccountName() + "!");
+        System.out.println("Welcome " + accountService.getUserName() + "!");
 
-        if(courses.isEmpty()) {
+        if(this.courses.isEmpty()) {
             System.out.println("You have not enrolled in any courses yet.");
         } else {
             System.out.println("You are enrolled in the following courses:");
-            for(int i = 1; i <= courses.size(); i++) {
-                System.out.println(i + ". " + courses.get(i - 1));
+            for(int i = 1; i <= this.courses.size(); i++) {
+                renderCourse(this.courses.get(i - 1));
             }
 
-            if (courses.size() != 0) {
+            if (this.courses.size() != 0) {
                 System.out.println("If you would like to see the details of a class please enter 'view'.");
             }
             System.out.println("If you would like to enroll in a class please enter 'enroll'.");
         }
     }
 
+    private void renderCourse(Course course) {
+        System.out.println("Course: " + course.getName() +  " goes here");
+    }
+
     @Override
     public String listen() {
         Scanner scanner = new Scanner(this.inputStream);
-        List<Course> courses = service.getCourses();
 
         while (true) {
             String input = scanner.nextLine();
@@ -64,13 +71,13 @@ public class StudentView extends AbstractView {
 
                     try {
 
-                        Course course = service.getCourses().get(index - 1);
+                        Course course = this.courses.get(index - 1);
                         System.out.println("You've selected: " + input + ". " + course.getName() + ". Is this correct? (y/n)");
 
                         input = scanner.nextLine();
                         if(input.equalsIgnoreCase("y")) {
                             scanner.close();
-                            service.viewCourse(course);
+                            courseService.selectCourse(course);
 
                             return "detail";
 
