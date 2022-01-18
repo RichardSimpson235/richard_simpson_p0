08205d9178e8a1fffa0ed783d0aa0b34ee07872f
Richard Simpson <richard.simpson235@gmail.com>
2022-01-18 14:21:26 -0500

    Even out length of spacers.

--- src/main/java/views/ApplicationQuitView.java
+++ src/main/java/views/ApplicationQuitView.java
@@ -10,9 +10,10 @@ public class ApplicationQuitView extends AbstractView {
 
     @Override
     public void render() {
+        System.out.println("====================================================================");
         System.out.println("Thank you for using the Course Management Portal, have a nice day!");
         System.out.println("END");
-        System.out.println("==========================================");
+        System.out.println("====================================================================");
     }
 
     // This method doesn't do anything in this class because we don't listen
